5c6af43b3dda3ab8f374badae057ad28adfe9bab
Peter McCready <peter.mccready@castlightfinancial.com>
2018-07-09 16:01:57 +0100

    override Devise Sessions destroy method and clear access_token cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,17 @@ class SessionsController < Devise::SessionsController
   # protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
   # override Devise Registrations controller to respond to json (for API)
   respond_to :json
+  skip_before_action :verify_signed_out_user, only: [:destroy]
+
+  def destroy
+    super
+    puts "destroying session"
+    clear_access_token
+  end
+
+  private
+
+  def clear_access_token
+    cookies.delete :access_token
+  end
 end
